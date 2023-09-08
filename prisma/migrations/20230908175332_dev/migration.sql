cf3eece8eb0a645da00144b0f677e1145f76b84d
Juninhoww2 <alqadianokira@gmail.com>
2023-09-08 13:57:31 -0400

    first commit

---
+++ prisma/migrations/20230908175332_dev/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "ingredients" TEXT NOT NULL,
+    "instructions" TEXT NOT NULL
+);
--- src/recipe-search/recipe.service.ts
+++ src/recipe-search/recipe.service.ts
@@ -1,27 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { Recipe } from './recipe.model';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class RecipeService {
-  private recipes: Recipe[] = [
-    {
-      id: 1,
-      name: 'Bolo de Chocolate',
-      ingredients: ['ovos', 'açúcar', 'chocolate'],
-      instructions: 'Passo a passo para fazer o bolo de chocolate...',
-    },
-    {
-      id: 2,
-      name: 'Pão de Queijo',
-      ingredients: ['polvilho', 'queijo', 'óleo'],
-      instructions: 'Passo a passo para fazer o pão de queijo...',
-    },
-  ];
+  private prisma: PrismaClient;
 
-  searchRecipes(query: string): Recipe[] {
-    const recipesFound = this.recipes.filter((recipe) =>
-      recipe.name.toLowerCase().includes(query.toLowerCase()),
-    );
-    return recipesFound;
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async searchRecipes(query: string) {
+    const recipes = await this.prisma.recipe.findMany({
+      where: {
+        name: { contains: query },
+      },
+    });
+    return recipes;
   }
 }
